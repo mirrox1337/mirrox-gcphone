31354bce9bba88080237a469b13f8f647af08fed
mirrox1337 <anton.per.larsson@gmail.com>
2020-06-08 16:28:23 +0200

    Add files via upload

---
+++ mirrox-gcphone/config.lua
@@ -0,0 +1,17 @@
+FixePhone = {
+  -- Poste de police
+  ['Polisen'] = { name =  "Polisen Reception", coords = { x = 441.2, y = -979.7, z = 30.58 } },
+  ['Akuten'] = { name =  "Sjukhus Reception", coords = { x = 310.61720, y = -593.04210, z = 43.25687 } },
+  ['Räddningstjänsten'] = { name =  "Räddningstjänsten Telefon", coords = { x = 201.98990, y = -1645.43500, z = 29.95895 } },
+  ['Securitas'] = { name =  "Securitas Reception", coords = { x = -1083.08700, y = -246.35540, z = 37.66312 } },
+  ['Bennys'] = { name =  "Bennys Telefon", coords = { x = -228.58740, y = -1329.63500, z = 30.99918 } },
+  ['Mekonomen'] = { name =  "Mekonomen Kontor", coords = { x = -352.77330, y = -132.12160, z = 38.91621 } },
+  ['Bilhandlaren'] = { name =  "Bilförsäljare Kontor", coords = { x = -30.54562, y = -1105.67100, z = 26.28919 } },
+  ['nightclub'] = { name =  "Hades Kontor", coords = { x = -1620.29900, y = -3010.30800, z = -75.40519 } },
+  ['forsakring'] = { name =  "Folksam Reception", coords = { x = -139.92, y = -633.2, z = 168.73 } },
+
+  -- Cabine proche du poste de police
+  ['112'] = { name = "Telefonkiosk", coords = { x = 372.25, y = -965.75, z = 28.58 } },
+}
+
+ShowNumberNotification = false -- Show Number or Contact Name when you receive new SMS
